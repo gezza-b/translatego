7afa713635376a5e86378c6993eef384029fd261
Gez <bachlmayr@gmail.com>
2018-09-08 17:25:20 +1000

    update build

--- src/index.go
+++ src/index.go
@@ -8,7 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 
-	//"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/service/comprehend"
 	"github.com/aws/aws-sdk-go/service/translate"
 )
